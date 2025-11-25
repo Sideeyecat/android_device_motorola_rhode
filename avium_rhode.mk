43b448e2854cfc121c3a36ef504ac09ae5ffc0c3
Sideeyecat <sohamasme3@gmail.com>
2025-11-25 22:37:58 +0530

    Update avium_rhode.mk

--- avium_rhode.mk
+++ avium_rhode.mk
@@ -8,8 +8,10 @@ AVIUM_VERSION_APPEND_TIME_OF_DAY = false
 
 # GMS
 WITH_GMS := true
-TARGET_GMS_TYPE := FULL
 
 
+# LatinIMEGooglePrebuilt
+TARGET_INCLUDE_GOOGLEIME := false
+TARGET_GOOGLEIME_OVERRIDE_IME := false
 
 AVIUM_MAINTAINER := helloisnotadevsadly
