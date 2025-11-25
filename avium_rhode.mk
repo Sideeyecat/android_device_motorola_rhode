23f152c06fbb847367fd6d3667b253c42a5985be
Sideeyecat <sohamasme3@gmail.com>
2025-11-25 22:25:45 +0530

    Create avium_rhode.mk

---
+++ avium_rhode.mk
@@ -0,0 +1,15 @@
+#
+# SPDX-FileCopyrightText: 2025 The AviumUI Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+AVIUM_BUILDTYPE = Unofficial
+AVIUM_VERSION_APPEND_TIME_OF_DAY = false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := FULL
+
+
+
+AVIUM_MAINTAINER := helloisnotadevsadly
